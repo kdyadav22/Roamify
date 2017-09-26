49bd518bd0ec18705bbeb0d190d8b73c0ecf03ef
Panalink-03\kapilyadav <kapil@panalinks.com>
2017-09-26 17:25:44 +0530

    Start a new innovation

---
+++ app/src/main/java/com/roamify/travel/activity/MenuPage.java
@@ -0,0 +1,15 @@
+package com.roamify.travel.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.roamify.travel.R;
+
+public class MenuPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_page);
+    }
+}
